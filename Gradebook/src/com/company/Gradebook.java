c552f865c0a7a32ac75f2156d1d3f8b5b2fcd34b
David Baker <dbaker@gatech.edu>
2019-03-10 17:09:29 -0400

    Completed gradebook - basic testing done

--- Gradebook/src/com/company/Gradebook.java
+++ Gradebook/src/com/company/Gradebook.java
@@ -9,11 +9,11 @@ public class Gradebook {
     //properties
     //list/array of sections
     ArrayList<Section> sections;
-    String currentSection;
+    String currentSectionName;
 
     public Gradebook(){
         sections = new ArrayList<>();
-        currentSection = null;
+        currentSectionName = "";
     }
 
     //methods
@@ -22,25 +22,50 @@ public class Gradebook {
     //to the section with the given sectionName.
     // Returns: true if it succeeds false if it fails(no section that matches that name).
     public boolean changeSection(String sectionName){
-       return false; //FIXME
+        String oldSection = currentSectionName;
+        currentSectionName = sectionName;
+        if(getCurSectionIndex() == -1){
+            currentSectionName = oldSection;
+            return false;
+        }
+       return true;
     }
 
     //creates a new section if there are not already 6 sections
     // and sectionName is not already used
     // Returns: true if it succeeds false if it fails.
     public boolean addSection(String sectionName){
-        return false; //FIXME
+        String oldSection = currentSectionName;
+        currentSectionName= sectionName;
+        if(getCurSectionIndex() == -1) {
+            sections.add(new Section(sectionName));
+            return true;
+        }
+        //do not add so do not modify current section
+        currentSectionName = oldSection;
+        return false;
     }
     //Creates a new student and adds the student to the currently active section
-    //returns: True if it succeeds, false if it fails(a student with the same username already exists)
+    //returns: True if it succeeds,
+    // false if it fails(a student with the same username already exists) or no Sections
     public boolean addStudent(String firstName, String lastName, String username, long phoneNumber){
-        return false; //FIXME
+        Section curSection = getCurSection();
+        if (curSection != null){
+            return curSection.addStudent(firstName, lastName, username, phoneNumber);
+        }
+        //currentSectionName is invalid
+        return false;
     }
 
     //returns overall score (total points earned for all assignments/ total points possible) as a percentage for specified student
-    //returns -1 if the student was not found or if the student did not have any assignments assigned.
+    //returns -1 if the student was not found or if the student did not have any assignments assigned. or no Sections
     public double getOverallScore(String username) {
-        return -1; //FIXME
+        Section curSection = getCurSection();
+        if (curSection != null){
+            return curSection.getOverallScore(username);
+        }
+        //currentSectionName is invalid
+        return -1;
     }
     //returns overall score as a percentage for the current section - (the average of each students overall score.)
     //Each student should have the same weight even if they have different number of points possible
@@ -48,15 +73,24 @@ public class Gradebook {
     //do not count students with no assignments assigned in the average.
     //returns -1 if there were no assignments in the section.
     public double getOverallScoreAvg(){
-        return -1; //FIXME
+        Section curSection = getCurSection();
+        if (curSection != null){
+            return curSection.getOverallScoreAvg();
+        }
+        //currentSectionName is invalid
+        return -1;
     }
-
 
     //creates a new assignment and adds it to the student specified by username.
     //Returns true if it succeeds and false if the student is not found or the student already has an assignment with the
     //same assignmentName
     public boolean addAssignmentToStudent(String username, String assignmentName, int pointsPossible){
-        return false; //FIXME
+        Section curSection = getCurSection();
+        if (curSection != null){
+            return curSection.addAssignmentToStudent(username, assignmentName, pointsPossible);//FIXME
+        }
+        //currentSectionName is invalid
+        return false;
     }
 
     //checks to make sure no student already has an assignment with the same assignment name first
@@ -64,50 +98,111 @@ public class Gradebook {
     // the students in the current section.
     //returns: True if successful and false if at least one student has the same assignment name
     public boolean addAssignmentToSection(String assignmentName, int pointsPossible){
-       return false;//FIXME
+        Section curSection = getCurSection();
+        if (curSection != null){
+            return curSection.addAssignmentToSection(assignmentName, pointsPossible);
+        }
+        //currentSectionName is invalid
+        return false;
     }
 
     //sets the pointsEarned property for the student specified in the current section for the assignment specified
     //returns: true if successful, false if the student is not found or the assignmentName is not found.
     public boolean setScore(String username, String assignmentName, int pointsEarned){
-        return false;//FIXME
+        Section curSection = getCurSection();
+        if (curSection != null){
+            return curSection.setScore(username,assignmentName, pointsEarned);
+        }
+        //currentSectionName is invalid
+        return false;
     }
 
     //returns score on assignment specified as a percentage for the specified student in the current section
     //returns -1 if the student was not found or the student did not have an assignment with the specified assignment name
     public double getAssignmentScorePercent(String username, String assignmentName){
-        return -1;//FIXME
+        Section curSection = getCurSection();
+        if (curSection != null){
+            return curSection.getAssignmentScorePercent(username,assignmentName);
+        }
+        //currentSectionName is invalid
+        return -1;
     }
 
     //returns the average score as a percentage for the current section.
     //only the student with the assignment assigned to them will be counted in the average.
     //returns -1 if no student had an assignment with the specified assignment name
     public double getAssignmentScoreAvg(String assignmentName){
-        return -1;//FIXME
+        Section curSection = getCurSection();
+        if (curSection != null){
+            return curSection.getAssignmentScoreAvg(assignmentName);
+        }
+        //currentSectionName is invalid
+        return -1;
     }
 
     //adds 1 to the specified student's tardy count.
     //returns: true if successful, false if the student was not found in the current section
     public boolean markTardy(String username) {
-        return false;//FIXME
+        Section curSection = getCurSection();
+        if (curSection != null){
+            return curSection.markTardy(username);
+        }
+        //currentSectionName is invalid
+        return false;
     }
 
     //adds 1 to the specified student's absent count.
     //returns: true if successful, false if the student was not found in the current section
     public boolean markAbsent(String username){
+        Section curSection = getCurSection();
+        if (curSection != null){
+            return curSection.markTardy(username);
+        }
+        //currentSectionName is invalid
         return false;
     }
 
     //returns the student tardy count for the specified student, it returns -1 if the student was not found.
     public int getTardyCount(String username){
-        return -1; //FIXME
+        Section curSection = getCurSection();
+        if (curSection != null){
+            return curSection.getTardyCount(username);
+        }
+        //currentSectionName is invalid
+        return -1;
     }
 
     //returns the student tardy count for the specified student, it returns -1 if the student was not found.
     public int getAbsentCount(String username){
-        return -1; //FIXME
+        Section curSection = getCurSection();
+        if (curSection != null){
+            return curSection.getAbsentCount(username);
+        }
+        //currentSectionName is invalid
+        return -1;
     }
 
+    //returns -1 if not found
+    private int getCurSectionIndex(){
+        int sectionIndex =-1;
+        for(int i=0; i <sections.size(); i++){
+            if(currentSectionName.equalsIgnoreCase(sections.get(i).getSectionName())){
+                sectionIndex =i;
+                break;
+            }
+        }
+        return sectionIndex;
+    }
+
+    //returns null if no current section found
+    private Section getCurSection(){
+        Section curSection = null;
+        int sectionIndex  = getCurSectionIndex();
+        if(sectionIndex != -1){
+            curSection = sections.get(sectionIndex);
+        }
+        return curSection;
+    }
 
 
 }
--- Gradebook/src/com/company/Section.java
+++ Gradebook/src/com/company/Section.java
@@ -7,10 +7,189 @@ import java.util.ArrayList;
  */
 public class Section {
     //list of students
-    ArrayList<Student> students;
-    String sectionName;
+    private ArrayList<Student> students;
+    private String sectionName;
 
+    public Section(String sectionName) {
+        this.sectionName = sectionName;
+        students = new ArrayList<>();
+    }
 
+    public String getSectionName() {
+        return sectionName;
+    }
 
+    //if username doesnt exsist add student
+    public boolean addStudent(String firstName, String lastName, String username, long phoneNumber){
+        if(getStudentIndex(username)==-1){
+            students.add(new Student(firstName, lastName, username, phoneNumber));
+            return true;
+        }
+        return false;
+    }
+
+    //returns -1 if no student found or no assignments for the student
+    public double getOverallScore(String username){
+        Student student = getStudent(username);
+        if (student != null){
+            return student.getOverallScore();
+        }
+        //no student with username found
+        return -1;
+
+    }
+    //returns -1 if no students found with assignments
+    //returns average overallScore for all students that have at least one assignment
+    public double getOverallScoreAvg(){
+        double overallScore = 0;
+        int numberStudents = 0;
+        //go through each student to see if to include in the total
+        for(int i=0; i<students.size(); i++){
+            double studentScore = students.get(i).getOverallScore();
+            //check to make sure they have at least one assignment
+            if(studentScore !=-1) {
+                overallScore = overallScore + studentScore;
+                numberStudents++;
+            }
+        }
+        //no students with an assignment found
+        if(numberStudents ==0) {
+            return -1;
+        }
+        return overallScore/numberStudents;
+    }
+
+    //returns false if no student found or assignment with assignmentName already exists for the student
+    public boolean addAssignmentToStudent(String username, String assignmentName, int pointsPossible){
+        Student student = getStudent(username);
+        if (student != null){
+            return student.addAssignment(assignmentName, pointsPossible);//FIXME
+        }
+        //no student with username found
+        return false;
+
+    }
+    //returns false if no student found or assignment with assignmentName already exists for the student
+    public boolean addAssignmentToSection(String assignmentName, int pointsPossible){
+        //go through each student to see if any student already contains an assignment with the name
+        for(int i=0; i<students.size(); i++){
+            if(students.get(i).assignmentNameIsUnique(assignmentName)){
+                return false;
+            }
+        }
+        //go back through all students and add the assignment
+        for (int i=0; i<students.size(); i++){
+            students.get(i).addAssignment(assignmentName,pointsPossible);
+        }
+        return true;
+
+    }
+    //sets the score for the specified student
+    //returns true if successful
+    //returns false if student was not found or assignment was not found
+    public boolean setScore(String username, String assignmentName, int pointsEarned){
+        Student student = getStudent(username);
+        if (student != null){
+            return student.setScore(assignmentName, pointsEarned);
+        }
+        //no student with username found
+        return false;
+
+    }
+    //returns score on assignment specified as a percentage for the specified student in the current section
+    //returns -1 if the student was not found or the student did not have an assignment with the specified assignment name
+    public double getAssignmentScorePercent(String username, String assignmentName){
+        Student student = getStudent(username);
+        if (student != null){
+            return student.getAssignmentScorePercent(assignmentName);
+        }
+        //no student with username found
+        return -1;
+    }
+
+    //returns -1 if no students found with assignments
+    //returns average percentage for all students that have the  assignment
+    public double getAssignmentScoreAvg(String assignmentName){
+        double overallScore = 0;
+        int numberStudents = 0;
+        //go through each student to see if to include in the total
+        for(int i=0; i<students.size(); i++){
+            double studentScore = students.get(i).getAssignmentScorePercent(assignmentName);
+            //check to make sure they have the assignment
+            if(studentScore !=-1) {
+                overallScore = overallScore + studentScore;
+                numberStudents++;
+            }
+        }
+        //no students with an assignment found
+        if(numberStudents ==0) {
+            return -1;
+        }
+        return overallScore/numberStudents;
+    }
+    //returns true if marked student successfully
+    //returns false if student was not found
+    public boolean markTardy(String username){
+        Student student = getStudent(username);
+        if (student != null){
+            student.markTardy();
+            return true;
+        }
+        //no student with username found
+        return false;
+    }
+    //returns true if marked student successfully
+    //returns false if student was not found
+    public boolean markAbsent(String username){
+        Student student = getStudent(username);
+        if (student != null){
+           student.markAbsent();
+           return true;
+        }
+        //no student with username found
+        return false;
+    }
+    //returns  number of days tardy
+    // returns -1 if student was not found
+    public int getTardyCount(String username){
+        Student student = getStudent(username);
+        if (student != null){
+            return student.getDaysTardy();
+        }
+        //no student with username found
+        return -1;
+    }
+    //returns number of days tardy
+    //returns -1 if student was not found
+    public int getAbsentCount(String username){
+        Student student = getStudent(username);
+        if (student != null){
+            return student.getDaysAbsent();
+        }
+        //no student with username found
+        return -1;
+    }
+
+    //returns -1 if username is not found otherwise returns index;
+    private int getStudentIndex(String userName){
+        int studentIndex =-1;
+        for(int i=0; i <students.size(); i++){
+            if(userName.equalsIgnoreCase(students.get(i).getUsername())){
+                studentIndex =i;
+                break;
+            }
+        }
+        return studentIndex;
+    }
+
+    //returns null if no current section found
+    private Student getStudent(String userName){
+        Student student = null;
+        int studentIndex  = getStudentIndex(userName);
+        if(studentIndex != -1){
+            student = students.get(studentIndex);
+        }
+        return student;
+    }
 
 }
